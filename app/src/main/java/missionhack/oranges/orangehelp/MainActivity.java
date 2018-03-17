d5314452fb97880a5432557eb62370f6b1e50b21
le_soleil <kvahidi@sfu.ca>
2018-03-17 00:34:36 -0700

    speech to text

--- app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
+++ app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
@@ -1,13 +1,51 @@
 package missionhack.oranges.orangehelp;
-
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView finalText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        finalText = (TextView) findViewById(R.id.thetext);
+    }
+    public void getSpeechInput(View view){
+
+        Intent intent  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if(intent.resolveActivity(getPackageManager() )!= null){
+            startActivityForResult(intent, 10);}
+        else{
+            Toast.makeText(this, "Your device does not support speech input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+
+            case 10 :
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    finalText.setText(result.get(0));
+                }
+                break;
+
+         }
     }
 }
