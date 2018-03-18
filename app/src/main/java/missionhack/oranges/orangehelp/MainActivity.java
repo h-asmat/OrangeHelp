a8dbf12891a671df2189b332a17044766ae207f8
le_soleil <kvahidi@sfu.ca>
2018-03-18 10:39:28 -0700

    build

--- app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
+++ app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
@@ -1,16 +1,9 @@
 package missionhack.oranges.orangehelp;
 
-import android.*;
 import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 
 import android.os.Build;
 import android.preference.PreferenceManager;
@@ -22,10 +15,8 @@ import android.view.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Button;
 import java.util.ArrayList;
 import java.util.Locale;
 
