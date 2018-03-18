033272c2e64f523b571cc87a30448f42497bd12e
hilal <hasmat@sfu.ca>
2018-03-17 21:26:25 -0700

    Beginning popup implementation

--- app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
+++ app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
@@ -1,12 +1,23 @@
 package missionhack.oranges.orangehelp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity implements OnAlertReceivedListener{
 
+    Point point;
     private static final String TAG = "MainActivity";
     FirebaseMessageReceiver messageReceiver;
     AlertSender alertSender;
@@ -16,16 +27,88 @@ public class MainActivity extends AppCompatActivity implements OnAlertReceivedLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_show = (Button) findViewById(R.id.show_popup);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                //Open popup window
+                if (point != null)
+                    showPopup(MainActivity.this, point);
+            }
+        });
         alert.setAlertReceivedListener(this);
         Log.d(TAG, "Creating message receiver");
         messageReceiver = new FirebaseMessageReceiver();
         createToken();
-        testSendingAlert();
+        try {
+            testSendingAlert();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    // Get the x and y position after the button is draw on screen
+// (It's important to note that we can't get the position in the onCreate(),
+// because at that stage most probably the view isn't drawn yet, so it will return (0, 0))
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+        int[] location = new int[2];
+        Button button = (Button) findViewById(R.id.show_popup);
+
+        // Get the x, y location and store it in the location[] array
+        // location[0] = x, location[1] = y.
+        button.getLocationOnScreen(location);
+
+        //Initialize the Point with x, and y positions
+        point = new Point();
+        point.x = location[0];
+        point.y = location[1];
     }
 
-    private void testSendingAlert() {
+    // The method that displays the popup.
+    private void showPopup(final Activity context, Point p) {
+        int popupWidth = 200;
+        int popupHeight = 150;
+
+        // Inflate the popup_layout.xml
+        LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.alert_popup_layout, viewGroup);
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+        // Some offset to align the popup a bit to the right, and a bit down, relative to button's position.
+        int OFFSET_X = 30;
+        int OFFSET_Y = 30;
+
+        // Clear the default translucent background
+        popup.setBackgroundDrawable(new BitmapDrawable());
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y + OFFSET_Y);
+
+        // Getting a reference to Close button, and close the popup when clicked.
+        Button close = (Button) layout.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                popup.dismiss();
+            }
+        });
+    }
+
+    private void testSendingAlert() throws InterruptedException {
         Log.d(TAG, "testSendingAlert: Creating AlertSender and sending it an alert");
         alertSender = new AlertSender();
+        Thread.sleep(5000);
         alertSender.sendAlert(alertSender.HILAL_TOKEN, Occupation.Fireman);
     }
 
