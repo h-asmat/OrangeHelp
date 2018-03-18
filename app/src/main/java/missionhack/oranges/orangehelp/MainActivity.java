58d878c5ed5d6112bf0e1e362af31efba63d1904
rudykong <rudykong2319@gmail.com>
2018-03-17 23:31:24 -0700

    added locations

--- app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
+++ app/src/main/java/missionhack/oranges/orangehelp/MainActivity.java
@@ -1,5 +1,14 @@
 package missionhack.oranges.orangehelp;
+
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.speech.RecognizerIntent;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +27,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         finalText = (TextView) findViewById(R.id.thetext);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        GPStracker g = new GPStracker(getApplicationContext());
+        Location l = g.getLocation();
+        if (l != null) {
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+            Toast.makeText(getApplicationContext(), "LAT: "+lat+"\n LON: "+lon, Toast.LENGTH_LONG).show();
+        }
     }
+
     public void getSpeechInput(View view){
 
         Intent intent  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
